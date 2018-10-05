c26e75dd4c55e6f1ec5dad2f7b97ad01cdb5f47f
Ariel Fogel <fogeltine@gmail.com>
2018-10-05 09:04:14 -0700

    add style loading into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
       }
     ]
   },
